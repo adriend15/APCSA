218d0dcef28343870655dfdd0b95fad9aa9bff7b
Adrien <adriendai3@gmail.com>
2024-09-25 12:36:11 -0400

    Finishe dmainjava

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int age = 15;
+        int people = 2;
+        double average = 3.8;
+        boolean isBirthday = false;
+        String name = "Adrien";
+
+        int totalAge = age*people;
+        System.out.println("the total age is " + name);
+    }
+
+}
